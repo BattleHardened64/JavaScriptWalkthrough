4dfa8d6cbf32bccab7d99c5c723eddf547019adb
BattleHardened64 <ewilliamstn001@gmail.com>
2022-08-31 17:59:24 -0500

    deleted some nonsense, fixed my functions

---
+++ app.js
@@ -0,0 +1,16 @@
+function gettime(){
+  document.getElementById("time").innerHTML = Date();
+}
+
+function warning(){
+  alert("you push a button!");
+}
+
+function animateskelyboi(){
+  var element = document.getElementById("animate");
+  
+
+
+
+  alert("you clik skele")
+}
