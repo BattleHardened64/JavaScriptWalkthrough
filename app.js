950836bac377261cddd7db4d5b6e1b6c0ad963a6
BattleHardened64 <ewilliamstn001@gmail.com>
2022-09-02 17:25:39 -0500

    fixed bunch of stuff

--- app.js
+++ app.js
@@ -5,12 +5,36 @@ function gettime(){
 function warning(){
   alert("you push a button!");
 }
-
+/*
 function animateskelyboi(){
-  var element = document.getElementById("animate");
-  
+  //var elem = document.getElementById("animate").style;
+  var img = $("#skele"),
+        width = img.get(0).width,
+        screenWidth = $(window).width(),
+        duration = 5000;
+
+    function animate() {
+        img.css("left", -width).animate({
+            "left": screenWidth
+        }, duration, animate);
+    }
+
+    animate();
+ //alert("you clik skele");
+ 
+}*/
 
 
-
-  alert("you clik skele")
+function playmusic(){
+var elem = document.getElementById("music");
+var num = Math.floor(Math.random() * 2);
+if (num === 1)
+{
+  var audio = new Audio("files/among-us-drip-audiotrimmer.mp3");
 }
+else{
+  var audio = new Audio("files/bluelobster.mp4");
+}
+audio.play();
+}
+
